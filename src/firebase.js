9217a94e43d0bd89fb2fdf72f8315d7c5c11d04c
code_mon <matrixzoho5@gmail.com>
2024-06-13 15:51:38 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgZ39HaMqmsKvGpJEY5E5lMEzwl0GzFyE",
+  authDomain: "code-mon0710.firebaseapp.com",
+  projectId: "code-mon0710",
+  storageBucket: "code-mon0710.appspot.com",
+  messagingSenderId: "557671234574",
+  appId: "1:557671234574:web:ec43cc6f3e322a4a55505f",
+  measurementId: "G-KQ3V8PE6GC"
+};
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
